e7949a8d8d79cc846a01cb881db038a1bacc12e2
Yunus Emre Gözen <72633131+emregozen37@users.noreply.github.com>
2025-10-15 16:23:04 +0300

    Add files via upload

---
+++ question3.cpp
@@ -0,0 +1,33 @@
+﻿// question3.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+	cout << "dizinin boyutunu giriniz = ";
+	cin >> n;
+	int *arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		cout << i+1 << ".elemani girin = ";
+		cin >> arr[i];
+	}
+	cout << "\nDizi elemanlari ve adresleri\n";
+	for (int i = 0; i < n; ++i) {
+		cout << "x[" << i << "] = " << arr[i] << ", adres = " << &arr[i] << endl;
+	}
+	return 0;
+}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
