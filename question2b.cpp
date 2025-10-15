59fde454178ff93621b3af877ee5f21af29700a3
Yunus Emre Gözen <72633131+emregozen37@users.noreply.github.com>
2025-10-15 16:04:46 +0300

    Add files via upload

---
+++ question2b.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cout << "integer bir sayi girin =  ";
+    cin >> x;
+    cout << "aralik girin = ";
+    cin >> y;
+
+    for (int i = 1; i <= y; ++i) {
+        cout << x << " * " << i << " = " << y * i << endl;
+    }
+}
