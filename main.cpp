811aa8d4e7ade28b0f6745cd58046bfaac592641
Yunus Emre Gözen <72633131+emregozen37@users.noreply.github.com>
2025-10-15 15:26:05 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int factorial(int n) {
+    if (n <= 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main()
+{
+    int number;
+    cout << "Bir sayi girin: ";
+    cin >> number;
+
+    if (number < 0) {
+        cout << "Negatif sayilarin faktoriyeli tanımsızdır." << endl;
+    }
+    else {
+        cout << number << "! = " << factorial(number) << endl;
+    }
+    return 0;
+}
