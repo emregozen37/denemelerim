2dc26e082a8286c34193679bd3a16e1d5b20ce20
Emre Gözen <emregozen5@gmail.com>
2025-10-15 23:16:34 +0300

    question4.cpp dosyası eklendi

---
+++ question4/question4.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int satir, sutun;
+    cout << "Satir sayisini giriniz: ";
+    cin >> satir;
+    cout << "Sutun sayisini giriniz: ";
+    cin >> sutun;
+
+    int* X = new int[satir * sutun];
+
+    for (int i = 0; i < satir; ++i) {
+        for (int j = 0; j < sutun; ++j) {
+            int idx = i * sutun + j;
+            
+            cout << "&X[" << i << "][" << j << "] = adres = " << X + idx << endl;
+        }
+    }
+
+    cout << "Toplam bellek miktari: " << satir * sutun * sizeof(int) << " byte" << endl;
+
+    delete[] X;
+    return 0;
+}
